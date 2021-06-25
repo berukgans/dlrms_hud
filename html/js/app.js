108699a2a2d0047287ab9729ca78f4f268921859
Delarmuss <83409265+Delarmuss@users.noreply.github.com>
2021-06-25 23:25:20 +0300

    Add files via upload

--- html/js/app.js
+++ html/js/app.js
@@ -9,23 +9,29 @@ $(function () {
     if (event.data.health <= 50) {
       $(".health-alert").css("display", "block");
       $(".health-alert").addClass("blink-anim");
+      $(".health").css("box-shadow", "0 0 0 rgba(0,0,0,1)");
     } else {
       $(".health-alert").css("display", "none");
       $(".health-alert").removeClass("blink-anim");
+      $(".health").css("box-shadow", "0px 0px 4px rgba(65, 155, 53, 1)");
     }
     if (event.data.hunger <= 25) {
       $(".hunger-alert").css("display", "block");
       $(".hunger-alert").addClass("blink-anim");
+      $(".hunger").css("box-shadow", "0 0 0 rgba(0,0,0,1)");
     } else {
       $(".hunger-alert").css("display", "none");
       $(".hunger-alert").removeClass("blink-anim");
+      $(".hunger").css("box-shadow", "0px 0px 4px rgba(210, 155, 67, 1)");
     }
     if (event.data.thirst <= 25) {
       $(".thirst-alert").css("display", "block");
       $(".thirst-alert").addClass("blink-anim");
+      $(".thirst").css("box-shadow", "0 0 0 rgba(0,0,0,1)");
     } else {
       $(".thirst-alert").removeClass("blink-anim");
       $(".thirst-alert").css("display", "none");
+      $(".thirst").css("box-shadow", "rgba(101, 137, 182, 1)");
     }
 
     if (event.data.vehicle == false) {
