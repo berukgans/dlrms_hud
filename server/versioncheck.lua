a726def51ad0767d785fc91afd3ccd73d2f6ee64
Delarmuss <83409265+Delarmuss@users.noreply.github.com>
2021-07-28 22:07:12 +0300

    Add files via upload

--- server/versioncheck.lua
+++ server/versioncheck.lua
@@ -1,18 +1,18 @@
 Citizen.CreateThread( function()
 updatePath = "/delarmuss/dlrms_hud" -- your git user/repo path
-resourceName = "dlrms_hud ("..GetCurrentResourceName()..")" -- the resource name
+resourceName = "("..GetCurrentResourceName()..")" -- the resource name
 
 function checkVersion(err,responseText, headers)
 	curVersion = LoadResourceFile(GetCurrentResourceName(), "version") -- make sure the "version" file actually exists in your resource root!
 
-	if curVersion < responseText then
+	if curVersion == responseText then 
+		print("\n"..resourceName.." is up to date, have fun!")
+	elseif curVersion < responseText then
 		print("\n###############################")
 		print("\n"..resourceName.." is outdated, should be:\n"..responseText.."\nis:\n"..curVersion.."\nplease update it from https://github.com"..updatePath.."")
 		print("\n###############################")
-	elseif curVersion > responseText then
-		print("You somehow skipped a few versions of "..resourceName.." or the git went offline, if it's still online i advise you to update ( or downgrade? )")
 	else
-		print("\n"..resourceName.." is up to date, have fun!")
+		print("You somehow skipped a few versions of "..resourceName.." or the git went offline, if it's still online i advise you to update ( or downgrade? )")
 	end
 end
 
