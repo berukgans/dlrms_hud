7a1851326be1430b3d183dec93121db505db259b
Delarmuss <83409265+Delarmuss@users.noreply.github.com>
2021-07-28 22:31:01 +0300

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,13 +1,12 @@
 fx_version 'adamant'
 game 'gta5'
-version '1.2.2'
+version '1.2.3'
 author 'https://github.com/Delarmuss'
 description 'Status hud by delarmuss'
 client_scripts {
 	'client/client.lua',
 	'config.lua'
 }
-server_script 'server/versioncheck.lua'
 ui_page'ui/ui.html'
 files{
 	"ui/css/*.css",
