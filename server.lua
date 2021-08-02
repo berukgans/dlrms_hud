35b46771f84956a8d93e86b0802333a9acc846ee
Delarmuss <83409265+Delarmuss@users.noreply.github.com>
2021-08-02 18:56:33 +0300

    Add files via upload

--- server.lua
+++ server.lua
@@ -6,14 +6,15 @@ ESX.RegisterServerCallback('dlrms_hud:getAccounts', function(source, cb)
   local xPlayer = ESX.GetPlayerFromId(source)
   local job = xPlayer.getJob()
   local mySociety = nil
+  local society = nil
     
   TriggerEvent('esx_society:getSociety', xPlayer.job.name, function(_society)
       mySociety = _society
   end)
-  
+
   if mySociety ~= nil then
       TriggerEvent('esx_addonaccount:getSharedAccount', mySociety.account, function(account)
-              society = account.money
+            society = account.money
       end)
   end
 
